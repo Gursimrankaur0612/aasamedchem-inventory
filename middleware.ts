e6b631cc87267a06d1ae227268c06430caf46a8c
Gursimran Kaur <1206simran@gmail.com>
2026-06-03 12:22:44 +0530

    fix middleware issue

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,6 @@
-import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function middleware(req: any) {
-  const token = await getToken({ req });
-
-  const isAdminRoute = req.nextUrl.pathname.startsWith(
-    "/admin"
-  );
-
-  if (isAdminRoute && token?.role !== "ADMIN") {
-    return NextResponse.redirect(
-      new URL("/login", req.url)
-    );
-  }
-
+export function middleware(req: NextRequest) {
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/admin/:path*"],
-};
